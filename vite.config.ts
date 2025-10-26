90a6eacbc3b83a4940e522beedf05f5672d1004a
arhsimluhar <rahul.mishra2003@gmail.com>
2025-10-26 21:10:12 +0530

    fixing config for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Set base for GitHub Pages repo at /portfolio-website/
+  base: '/portfolio-website/',
   plugins: [react()],
 })
-
